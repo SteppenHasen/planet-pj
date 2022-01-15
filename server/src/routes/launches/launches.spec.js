2b5ca1f01fd7167c81d60584ee4705b5c17ff430
SteppenHasen <dustasura@gmail.com>
2022-01-15 20:56:13 +0200

    Package-json changes

---
+++ server/src/routes/launches/launches.spec.js
@@ -0,0 +1,48 @@
+const request = require('supertest')
+const app = require('../../app')
+
+describe('Test GET /launches', () => {
+    test('It should respond with 200 success', async () => {
+        const response = await request(app)
+        .get('/launches')
+        .expect('Content-Type', /json/)
+        .expect(200)
+    })
+})
+
+describe('Test POST /launches', () => {
+    const completeLaunchData = {
+        mission: "USS Enterprise",
+        rocket: 'NCC 1701-D',
+        target: 'Kepler',
+        launchDate: 'January 4, 2024'
+    }
+
+    const launchDataWithoutDate = {
+        mission: "USS Enterprise",
+        rocket: 'NCC 1701-D',
+        target: 'Kepler'
+    }
+
+    test('It should respond with 201 created', async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send(completeLaunchData)
+            .expect('Content-Type', /json/)
+            .expect(201);
+        
+        const requestDate = new Date(completeLaunchData.launchDate).valueOf()
+        const responseDate = new Date(response.body.launchDate).valueOf()
+        expect(responseDate).toBe(requestDate)
+
+        expect(response.body).toMatchObject({launchDataWithoutDate})
+    })
+
+    test('It should catch missing required properties', () => {
+        
+    })
+
+    test('It should catches invalid dates', () => {
+        
+    })
+})
